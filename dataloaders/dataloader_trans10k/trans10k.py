badad0376c3cb70a5f7f34eb088082e9aca481dc
Logan Thomson <xycoord@gmail.com>
2025-01-30 12:16:38 +0000

    Refactor Dataloader to use pathlib

--- dataloaders/dataloader_trans10k/trans10k.py
+++ dataloaders/dataloader_trans10k/trans10k.py
@@ -1,10 +1,10 @@
 """Prepare Trans10K dataset"""
-import os
 import torch
 import numpy as np
 import logging
 
 from PIL import Image
+from pathlib import Path
 
 from .settings import cfg
 
@@ -34,33 +34,45 @@ class TransSegmentation(object):
     def __init__(self, root='./datasets/Trans10K', split='train', difficulty='mix', mode=None, transform=None, augmentation=None, ignore_class=True):
         super(TransSegmentation, self).__init__()
 
-        self.root = os.path.join(cfg.ROOT_PATH, root)
-        assert os.path.exists(self.root), "Please put dataset in {SEG_ROOT}/datasets/Trans10K"
+        self.root = Path(cfg.ROOT_PATH) / root
+        assert self.root.exists(), "Please put dataset in {SEG_ROOT}/datasets/Trans10K"
 
         self.split = split
         self.mode = mode if mode is not None else split
-
         self.transform = transform
         self.augmentation = augmentation 
-
         self.ignore_class = ignore_class
 
+        self.image_paths, self.mask_paths = _get_trans10k_pairs(self.root, self.split, difficulty=difficulty)
 
-        self.images, self.mask_paths = _get_trans10k_pairs(self.root, self.split, difficulty=difficulty)
+        assert (len(self.image_paths) == len(self.mask_paths))
 
-        assert (len(self.images) == len(self.mask_paths))
-
-        if len(self.images) == 0:
+        if len(self.image_paths) == 0:
             raise RuntimeError("Found 0 images in subfolders of:" + root + "\n")
 
-        if ignore_class:
-            self.valid_classes = [0,1]
-            self._key = np.array([0,1])
-        else:
-            self.valid_classes = [0,1,2]
-            self._key = np.array([0,1,2])
-        
+        self.valid_classes = [0,1,2] if not ignore_class else [0,1]
+        self._key = np.array(self.valid_classes)
         self._mapping = np.array(range(-1, len(self._key) - 1)).astype('int32') + 1
+
+    
+    def __getitem__(self, index):
+        img = Image.open(self.image_paths[index]).convert('RGB')
+        mask = Image.open(self.mask_paths[index])
+
+        if img.size[0] != mask.size[0] or img.size[1] != mask.size[1]:
+            raise Exception(f'Image and mask size do not match: {self.image_paths[index]}, {img.size}, {mask.size}')
+
+        img = np.array(img)
+        mask = np.array(mask)
+        mask = self._preprocess_mask(mask)
+
+        # Augmentation and Normalization
+        augmented = self.augmentation(image=img, mask=mask)
+        augmented = self.transform(image=augmented['image'], mask=augmented['mask'])
+        img = augmented['image'].to(torch.float32)
+        mask = augmented['mask'].to(torch.float32)
+
+        return img, mask, self.image_paths[index].name
 
 
     def _class_to_index(self, mask):
@@ -73,15 +85,8 @@ class TransSegmentation(object):
         return self._key[index].reshape(mask.shape)
 
 
-    def __getitem__(self, index):
-        img = Image.open(self.images[index]).convert('RGB')
-        mask = Image.open(self.mask_paths[index])
-
-        if img.size[0] != mask.size[0] or img.size[1] != mask.size[1]:
-            raise Exception(f'Image and mask size do not match: {self.images[index]}, {img.size}, {mask.size}')
-
-        img = np.array(img)
-        mask = np.array(mask)[:,:,:3].mean(-1)
+    def _preprocess_mask(self, mask):
+        mask = mask[:,:,:3].mean(-1)
 
         # Things and Stuff
         if self.ignore_class:
@@ -92,20 +97,11 @@ class TransSegmentation(object):
             mask[mask==255.0] = 2 
             assert mask.max()<=2, mask.max()
 
-        # Augmentation and Normalization
-        augmented = self.augmentation(image=img, mask=mask)
-        augmented = self.transform(image=augmented['image'], mask=augmented['mask'])
-        img = augmented['image'].to(torch.float32)
-        mask = augmented['mask'].to(torch.float32)
+        return mask
 
-        return img, mask, os.path.basename(self.images[index])
 
     def __len__(self):
-        return len(self.images)
-
-    def get_img_size(self):
-        return 512, 512
-
+        return len(self.image_paths)
 
     @property
     def pred_offset(self):
@@ -127,38 +123,38 @@ class TransSegmentation(object):
     
 
 def _get_trans10k_pairs(folder, split='train', difficulty='mix'):
+    folder = Path(folder)
 
     def get_path_pairs(img_folder, mask_folder):
         img_paths = []
         mask_paths = []
-        imgs = os.listdir(img_folder)
 
-        for imgname in imgs:
-            imgpath = os.path.join(img_folder, imgname)
-            maskname = imgname.replace('.jpg', '_mask.png')
-            maskpath = os.path.join(mask_folder, maskname)
-            if os.path.isfile(imgpath) and os.path.isfile(maskpath):
-                img_paths.append(imgpath)
-                mask_paths.append(maskpath)
-            else:
-                logging.info('cannot find the mask or image:', imgpath, maskpath)
+        for img_path in img_folder.iterdir():
+            if img_path.suffix.lower() == '.jpg':
+                mask_path = mask_folder / (img_path.stem + '_mask.png')
+                if img_path.is_file() and mask_path.is_file():
+                    img_paths.append(img_path)
+                    mask_paths.append(mask_path)
+                else:
+                    logging.info('cannot find the mask or image:', img_path, mask_path)
 
         logging.info('Found {} images in the folder {}'.format(len(img_paths), img_folder))
         return img_paths, mask_paths
 
 
     if split == 'train':
-        img_folder = os.path.join(folder, split, 'images')
-        mask_folder = os.path.join(folder, split, 'masks')
+        img_folder  = folder / split / 'images'
+        mask_folder = folder / split / 'masks'
         img_paths, mask_paths = get_path_pairs(img_folder, mask_folder)
     else:
-        assert split == 'validation' or split == 'test'
-        easy_img_folder = os.path.join(folder, split, 'easy', 'images')
-        easy_mask_folder = os.path.join(folder, split, 'easy', 'masks')
-        hard_img_folder = os.path.join(folder, split, 'hard', 'images')
-        hard_mask_folder = os.path.join(folder, split, 'hard', 'masks')
+        assert split in ['validation', 'test']
+        easy_img_folder  = folder / split / 'easy' / 'images'
+        easy_mask_folder = folder / split / 'easy' / 'masks'
+        hard_img_folder  = folder / split / 'hard' / 'images'
+        hard_mask_folder = folder / split / 'hard' / 'masks'
         easy_img_paths, easy_mask_paths = get_path_pairs(easy_img_folder, easy_mask_folder)
         hard_img_paths, hard_mask_paths = get_path_pairs(hard_img_folder, hard_mask_folder)
+
         if difficulty == 'easy':
             return easy_img_paths, easy_mask_paths
         elif difficulty == 'hard':
